38a2f202ee53753ca89a205bcdb40900c5897713
Yoscroll <ianhenry.c808@gmail.com>
2018-03-07 13:56:39 -1000

    Test Done

---
+++ app.js
@@ -0,0 +1,20 @@
+var objects={
+	"Naked Snake's Box...",
+	"The Cube...",
+	"Diamond...",
+	"The D..."
+};
+objects.splice(0,1,2);
+objects.push("Keegan");
+var 1 = "is the best weapon for deception...";
+
+var Tribute#One={
+	firstName:"Keegan",
+	lastName:"(Like I Care, its long)",
+	age:"I really don't know LOL",
+}
+
+
+//I am not keen with JS at all, html and css I can work around, js for me is slow and difficult...
+//Not as an excuse, I just admit that I suck at js.
+//Just to help you out, I missed 13 and 15.
